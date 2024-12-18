58c792e3b190bfec25af182aac45d48c1d9a5a71
mg95 <66237187+themg95@users.noreply.github.com>
2024-12-18 11:41:39 +0300

    switch to using actions i hope it workss noww >~<

--- svelte.config.js
+++ svelte.config.js
@@ -4,20 +4,6 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/meow' : ''
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				console.warn(`Failed to prerender ${path} (referrer: ${referrer}): ${message}`);
-				if (path === '/') {
-					// Suppress the error for root route or take custom action
-					return;
-				}
-				throw new Error(message);
-			},
-			entries: ['/']
-		}
 	}
 };
 
