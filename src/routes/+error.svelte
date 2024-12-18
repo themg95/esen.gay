e13e39c5f0759c872872d9575cbac0c1046e2893
mg95 <66237187+themg95@users.noreply.github.com>
2024-12-18 11:29:43 +0300

    meow add a temp site meoowwww i hope it workss

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<p>Uh oh, you found an error </p>
+
+<p>{page.status}: {page.error.message}</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,24 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/meow' : ''
+		},
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				console.warn(`Failed to prerender ${path} (referrer: ${referrer}): ${message}`);
+				if (path === '/') {
+					// Suppress the error for root route or take custom action
+					return;
+				}
+				throw new Error(message);
+			},
+			entries: ['/']
+		}
+	}
 };
 
 export default config;
