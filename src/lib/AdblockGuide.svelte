69bf860dad148b227e36123518109ef76c6fb3ea
mg95 <66237187+themg95@users.noreply.github.com>
2025-01-07 10:10:38 +0300

    finally finish the adblock guide

--- src/lib/AdblockGuide.svelte
+++ src/lib/AdblockGuide.svelte
@@ -17,9 +17,22 @@
   <h1 class="text-3xl">Here is the recommendation for you:</h1>
   {#if bowserParser.getOSName().toLowerCase() === "android"}
     <AndroidGuide />
-  {:else if (bowserParser.getOSName().toLowerCase() === "ios")}
+  {:else if bowserParser.getOSName().toLowerCase() === "ios"}
     <p>Download AdGuard and use Safari</p>
-    <a href="https://apps.apple.com/us/app/adguard-adblock-privacy/id1047223162">Click to download AdGuard</a>
+    <a href="https://apps.apple.com/us/app/adguard-adblock-privacy/id1047223162"
+      >Click to download AdGuard</a
+    >
+  {:else if bowserParser.getEngineName().toLowerCase() === "blink"}
+    <p>Download uBlock Origin Lite</p>
+    <a
+      href="https://chromewebstore.google.com/detail/ublock-origin-lite/ddkjiahejlhfcafbddmgiahcphecmpfh"
+      >Click to download uBlock Origin Lite</a
+    >
+  {:else if bowserParser.getEngineName().toLowerCase() === "gecko"}
+    <p>Download uBlock Origin</p>
+    <a href="https://addons.mozilla.org/en-GB/firefox/addon/ublock-origin/"
+      >Click to download uBlock Origin</a
+    >
   {:else}
     <h2 class="text-2xl">Your browser/OS is not currently supported... Sorry.</h2>
   {/if}
--- src/lib/AndroidGuide.svelte
+++ src/lib/AndroidGuide.svelte
@@ -19,12 +19,15 @@
     <h2 class="text-2xl">Select the one that applies:</h2>
     <button onclick={imSmart}>I can download APKs</button>
     <button onclick={imNormal}>Just give me something that works</button>
-  {:else if (stage === 1)}
+  {:else if stage === 1}
     <a href="https://github.com/uazo/cromite/releases/latest">Download Cromite</a>
     <p>requires messing with github and apks</p>
-  {:else if (stage === 2)}
+  {:else if stage === 2}
     <a href="https://play.google.com/store/apps/details?id=com.brave.browser">Download Brave</a>
-    <p class="text-red">⚠ WARNING: has crypto bullshit and a homophobic ceo. Also uses the brave search engine by default. ⚠</p>
+    <p class="text-red">
+      ⚠ WARNING: has crypto bullshit and a homophobic ceo. Also uses the brave search engine by
+      default. ⚠
+    </p>
   {/if}
 </div>
 {#if stage !== 0}
